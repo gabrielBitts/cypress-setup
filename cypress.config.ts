3cd6fc8e7412f487d8168f7530ee3179ab0e9e25
Gabriel Nattis <gabrielbits@live.com>
2025-02-22 17:26:57 -0300

    Initial repository setup

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress'
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  retries: 2,
+  reporter: 'spec',
+  e2e: {
+    baseUrl: 'https://start.duckduckgo.com/',
+    specPattern: 'cypress/e2e/*.cy.{js, jsx, ts}',
+    testIsolation: false,
+  },
+})
